1d14a17491349975516028cab793daee08e970a1
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-21 17:43:05 +0800

    Defined mongoose.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const add = express();
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://')
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
