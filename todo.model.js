d5a999b621fd393caaab1f6392418542c76eed02
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-21 18:22:32 +0800

    Defined Todo.

--- todo.model.js
+++ todo.model.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+let Todo = new Schema({
+    todo_description: {
+        type: String
+    },
+    todo_responsible: {
+        type: String
+    }
+})
