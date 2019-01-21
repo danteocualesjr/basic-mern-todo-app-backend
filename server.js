966d52bab6fe9ac543c4625de32e93cb351c6fe8
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2019-01-21 17:31:58 +0800

    Attached cors and body-parser.

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const add = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
